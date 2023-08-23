08dcf04a15acc4387d730afff8201e633150f7d3
Eric-Wubbo Lameijer <eric_wubbo@hotmail.com>
2023-08-23 17:44:15 +0200

    Added comment, cleaned up code

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,7 @@
 package org.example;
 
 public class Main {
+    // Program to greet someone, nicely formatted
     public static void main(String[] args) {
         var text = "Hello GitTest!";
         var blockSize = text.length();
@@ -10,7 +11,7 @@ public class Main {
     }
 
     private static void displaySquare(int size) {
-        for (int row=0; row<size; row++) {
+        for (int row = 0; row < size; row++) {
             for (int column = 0; column < size; column++) System.out.print("*");
             System.out.println();
         }
