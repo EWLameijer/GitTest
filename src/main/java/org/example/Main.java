f00bd61f4068809affdbc9b2bab4d71627a701fe
Eric-Wubbo Lameijer <eric_wubbo@hotmail.com>
2023-08-23 16:46:20 +0200

    Now asks for the user's name

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,16 +1,29 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        var text = "Hello GitTest!";
+        var name = getName();
+        var text = "Hello " + name + "!";
         var blockSize = text.length();
         displaySquare(blockSize);
-        System.out.println("Hello GitTest!");
+        System.out.println(text);
         displaySquare(blockSize);
     }
 
+    private static String getName() {
+        System.out.print("Please enter your name: ");
+        Scanner in = new Scanner(System.in);
+        String name = "";
+        do {
+            name = in.nextLine();
+        } while (name.isBlank());
+        return name;
+    }
+
     private static void displaySquare(int size) {
-        for (int row=0; row<size; row++) {
+        for (int row = 0; row < size; row++) {
             for (int column = 0; column < size; column++) System.out.print("*");
             System.out.println();
         }
