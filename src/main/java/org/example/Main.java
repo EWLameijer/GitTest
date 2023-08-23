958c1b829dd0b59eaafda446365a0e1fe5560b6f
Eric-Wubbo Lameijer <eric_wubbo@hotmail.com>
2023-08-23 15:08:25 +0200

    Added blocks above and below text

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,17 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-
+        var text = "Hello GitTest!";
+        var blockSize = text.length();
+        displaySquare(blockSize);
         System.out.println("Hello GitTest!");
+        displaySquare(blockSize);
+    }
+
+    private static void displaySquare(int size) {
+        for (int row=0; row<size; row++) {
+            for (int column = 0; column < size; column++) System.out.print("*");
+            System.out.println();
+        }
     }
 }
